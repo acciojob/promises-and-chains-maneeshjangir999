65b4aab46bf68329b162aa3c537aea1145039ccd
Maneesh Jangir <122728056+maneeshjangir999@users.noreply.github.com>
2025-03-06 11:48:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+let age = document.getElementById("age").value
+let btn = document.getElementById("btn")
+let name = document.getElementById("name").value
+
+btn.addEventListener("click",()=>{
+
+    new Promise((resolve,reject)=>{
+
+           if(age>18){
+			   setTimeout(()=>{
+				      resolve(`Welcome, ${name}. You can vote.`)
+			   },4000)
+		   }
+		   else{
+			   setTimeout(()=>{
+				       reject(`Oh sorry ${name}. You aren't old enough`)
+			   },4000)
+		   }
+		
+	}).then((res)=>{
+		alert(res)
+	}).catch((res)=>{
+      alert(res)
+	})	
+
+	
+	
+})
